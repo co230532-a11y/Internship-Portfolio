8ff91d2492b630522a378bd8197081528f8f5635
Khan <ferzkhan668@gmail.com>
2026-06-08 17:14:02 -0200

    chore: add github actions deployment pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  site: 'https://co230532-a11y.github.io',
+  base: '/internship-portfolio',
 });
